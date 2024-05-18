3ceb4020aa19c9f3998ab4ab3be32eb7d20a5db4
lovechino <quochung13122002@gmail.com>
2024-05-18 13:36:06 +0700

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
 
 app.listen(3000,()=>{
     console.log("ngu vch")
 })
+
+mongoose.connect("mongodb+srv://admin:achinchin2kx@cluster0.xelldvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(()=>console.log("da ket noi"))
+.catch(()=>console.log("ket noi loi"))
