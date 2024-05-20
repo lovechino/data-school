f7af50e0664d94b98f04cc73662b38f6bb7ec23f
lovechino <quochung13122002@gmail.com>
2024-05-20 23:32:09 +0700

    good night

---
+++ Controllers/User.controller.js
@@ -0,0 +1,16 @@
+const Student = require("../Schemas/Student.model")
+const Teacher = require("../Schemas/Teacher.model")
+
+const createUser = async(req,res)=>{
+
+}
+
+const loginUser = async(req,res)=>{
+
+}
+
+
+module.exports = {
+    createUser,
+    loginUser
+}
