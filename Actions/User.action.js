32b81ad185e6546bce54a322ad0e0c59c236aa04
lovechino <quochung13122002@gmail.com>
2024-05-23 15:57:45 +0700

    ilovegithub

--- Actions/User.action.js
+++ Actions/User.action.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const{createUser,createT} = require("../Controllers/User.controller")
+const{createUser,createT,loginUser} = require("../Controllers/User.controller")
 const{authAd,authenToken} = require("../Controllers/JWT")
 
 //create hs
 router.post('/',authAd,createUser)
 
 //login
-// router.post('/login',loginUser)
+router.post('/login',loginUser)
 
 //create gv
 router.post('/teacher',authAd,createT)
